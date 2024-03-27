5db0a0b40fa1f96cb62b25e57b149e41f9a30218
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2024-03-27 22:41:59 +0000

    Begin refactoring `strategies.py` to handle `capybot` changes

--- strategies.py
+++ strategies.py
@@ -2,12 +2,13 @@
 # coding: utf-8
 
 import matplotlib.pyplot as plt
-import datetime as dt
 import matplotlib.dates as md
 import matplotlib.animation as animation
 from capybot import load_data
 import sys
 from matplotlib.widgets import Slider
+
+ARBITRAGE_RELATIVE_LIMIT = 1.0005
 
 # Do not use automatic offsets for plot axis
 import matplotlib as mpl
@@ -20,33 +21,96 @@ file = sys.argv[1]
 
 data = load_data(file);
 number_of_strategies = len(data['strategies']);
-fig, ax = plt.subplots(number_of_strategies, 1, figsize = (6, 10));
+
+fig, ax = plt.subplots(nrows = number_of_strategies, ncols = 1, figsize = (6, 10));
 fig.tight_layout(pad=2.0)
 
 def animate_strategies(j):
     data = load_data(file);
+
+    # if only one arbitrage strategy is being used, the ax object is not iterable,
+    # and will not be subscriptable - thus the checked
+    if number_of_strategies > 1:
+        ax_object = ax[i]
+    else:
+        ax_object = ax
+
     for i, uri in enumerate(data['strategies']):
         strategy = data['strategies'][uri]
-        ax[i].clear()
-        ax[i].set_title(strategy['parameters']['name'] + "")
+        ax_object.clear()
+        ax_object.set_title(strategy['parameters']['name'] + "")
+
+        ax_object.set_xlabel('Time (s)')
+        ax_object.set_ylabel('Arbitrage potential')
+
+        # A horizontal line is drawn at the relative limit at which arbitrage is considered
+        ax_object.axhline(y = ARBITRAGE_RELATIVE_LIMIT, color = 'black', linestyle = '--', label = 'Arbitrage threshold')
 
         timestamps = strategy['statuses']['time']
-        dates = [dt.datetime.fromtimestamp(ts) for ts in timestamps]
+        start_ts = timestamps[0]
+
+        timestamps = [ts - start_ts for ts in timestamps]
 
         if len(strategy['statuses']['value']) == 0:
             break
 
         for key in strategy['statuses']['value'][0]:
             y = list(map(lambda x: x[key], strategy['statuses']['value']))
-            ax[i].plot(dates, y, label = key)
-        
+            ax_object.plot(timestamps, y, label = key)
+
         # A trade order is indicated by a vertical red line
         if uri in data['orders']:
-            x = [dt.datetime.fromtimestamp(ts) for ts in data['orders'][uri]['time']];
-            for xi in x:
-                ax[i].axvline(x = xi, color = 'r')
-        
-        ax[i].legend();    
+            order_ts = [ts - start_ts for ts in data['orders'][uri]['time']];
+            ax_object.axvline(color = 'r', label = 'Trade order')
+            for order_t in order_ts:
+                ax_object.axvline(x = order_t, color = 'r', linestyle = '-.', alpha = 0.05)
 
-anim = animation.FuncAnimation(fig, animate_strategies, interval = 1000)
+        ax_object.legend();
+
+""" def animate_strategies(j):
+    if number_of_strategies > 1:
+        return "More than 1 strat, currently not implemented"
+
+    data = load_data(file);
+
+    for i, uri in enumerate(data['strategies']):
+
+        # the entirety of the strategies' collected data
+        strategy = data['strategies'][uri]
+
+        ax.clear()
+        ax.set_title(strategy['parameters']['name'] + "")
+
+        print("strat name: ", strategy['parameters']['name'])
+
+        timestamps = strategy['statuses']['time']
+        min_timestamp = timestamps[0]
+
+        timestamps = [ts - min_timestamp for ts in timestamps]
+        print(timestamps)
+
+        if len(strategy['statuses']['value']) == 0:
+            break
+
+        arbitrage_vals = list(map(lambda x: x['arbitrage'], strategy['statuses']['value']))
+        print(arbitrage_vals)
+        rev_arbitrage_vals = list(map(lambda x: x['reverse'], strategy['statuses']['value']))
+        print(rev_arbitrage_vals)
+
+        ax.plot(timestamps, arbitrage_vals, label = "arbitrage", color = 'b')
+        ax.plot(timestamps, rev_arbitrage_vals, label = "reverse arbitrage", color = 'g')
+
+#        # A trade order is indicated by a vertical red line
+#        if uri in data['orders']:
+#            min_order_ts = min(data['orders'][uri]['time'])
+#
+#            order_t = [ts - min_order_ts for ts in data['orders'][uri]['time']];
+#            for oi in order_t:
+#                #print("order at: ", xi)
+#                ax.axvline(x = oi, color = 'r')
+
+        ax.legend(); """
+
+
+anim = animation.FuncAnimation(fig, func = animate_strategies, interval = 1000)
 plt.show()
