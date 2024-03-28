a89343bc0c78dca57c56c014a38b978568945101
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2024-03-28 02:39:26 +0000

    Add note regarding new static/dyn switch for `strategies.py`

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # capybot-monitor
 
 ## Overview
-The Capybot Monitor is a collection of Python scripts to monitor the status of a running instance of a [Capybot trading bot](https://github.com/MystenLabs/capybot). It produces plots of the price development and key status indicators used by Capybot. The plots are updated live.
+The Capybot Monitor is a collection of Python scripts to monitor the status of a running instance of a
+[Capybot trading bot](https://github.com/MystenLabs/capybot).
+
+It produces plots of the price development and key status indicators used by Capybot. The plots are updated live.
 
 ## Installation and Running
-First download and install the Capybot trading bot as instructed in the [Capybot repository](https://github.com/MystenLabs/capybot). Run the Capybot and store the output to a file
+First download and install the Capybot trading bot as instructed in the [Capybot repository](https://github.com/MystenLabs/capybot).
+Run the Capybot and store the output to a file
 ```
 npm run start > capybot.log
 ```
@@ -12,9 +16,18 @@ Now, first clone this repo. Run the `pools.py` script and give the path to the l
 ```
 python3 pools.py ../capybot/capybot.log
 ```
-This should show a window showing the development of the amount of coin B you will get for fixed amount coin A relative to when the Capybot was started.
+This should show a window showing the development of the amount of coin B you will get for fixed amount coin A relative
+to when the Capybot was started.
+
 <img src="./images/pools.png" alt="pools">
 
-The `strategies.py` script is started in the same way but produces plots of relevant variables for the trading strategies the Capybot is using. A red vertical line shows that the given strategy gave a trade order at this point in time.
+The `strategies.py` script is started in the same way, but produces plots of relevant variables for the trading
+strategies the Capybot is using.
+
+In addition to the log file, it takes in a second argument that is either `--static` or `--dynamic`, depending on
+whether the plot is to be updated by `matplotlib`, or rendered statically without refreshing.
+This useful for large log files that are impractical to reprocess every tick.
+
+A red vertical line shows that the given strategy gave a trade order at this point in time.
 
 <img src="./images/strategies.png" alt="strategies">
--- strategies.py
+++ strategies.py
@@ -6,7 +6,6 @@ import matplotlib.dates as md
 import matplotlib.animation as animation
 from capybot import load_data
 import sys
-from matplotlib.widgets import Slider
 
 ARBITRAGE_RELATIVE_LIMIT = 1.0005
 
@@ -14,7 +13,7 @@ ARBITRAGE_RELATIVE_LIMIT = 1.0005
 import matplotlib as mpl
 mpl.rcParams['axes.formatter.useoffset'] = False
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     sys.exit("No path to Capybot log file given")
 
 file = sys.argv[1]
@@ -26,6 +25,8 @@ fig, ax = plt.subplots(nrows = number_of_strategies, ncols = 1, figsize = (6, 10
 fig.tight_layout(pad=2.0)
 
 def animate_strategies(j):
+    # Recall that this is going to be done once a second - visualizing logs from long-running
+    # instances should be done in a static manner, without animation
     data = load_data(file);
 
     # if only one arbitrage strategy is being used, the ax object is not iterable,
@@ -49,8 +50,10 @@ def animate_strategies(j):
         timestamps = strategy['statuses']['time']
         start_ts = timestamps[0]
 
+        # Convert UNIX timestamps to seconds since the start of the simulation
         timestamps = [ts - start_ts for ts in timestamps]
 
+        # No recorded statuses, skip
         if len(strategy['statuses']['value']) == 0:
             break
 
@@ -61,56 +64,20 @@ def animate_strategies(j):
         # A trade order is indicated by a vertical red line
         if uri in data['orders']:
             order_ts = [ts - start_ts for ts in data['orders'][uri]['time']];
+            # Set the label for all trade orders once
             ax_object.axvline(color = 'r', label = 'Trade order')
             for order_t in order_ts:
                 ax_object.axvline(x = order_t, color = 'r', linestyle = '-.', alpha = 0.05)
 
         ax_object.legend();
 
-""" def animate_strategies(j):
-    if number_of_strategies > 1:
-        return "More than 1 strat, currently not implemented"
+switch = sys.argv[2]
 
-    data = load_data(file);
-
-    for i, uri in enumerate(data['strategies']):
-
-        # the entirety of the strategies' collected data
-        strategy = data['strategies'][uri]
-
-        ax.clear()
-        ax.set_title(strategy['parameters']['name'] + "")
-
-        print("strat name: ", strategy['parameters']['name'])
-
-        timestamps = strategy['statuses']['time']
-        min_timestamp = timestamps[0]
-
-        timestamps = [ts - min_timestamp for ts in timestamps]
-        print(timestamps)
-
-        if len(strategy['statuses']['value']) == 0:
-            break
-
-        arbitrage_vals = list(map(lambda x: x['arbitrage'], strategy['statuses']['value']))
-        print(arbitrage_vals)
-        rev_arbitrage_vals = list(map(lambda x: x['reverse'], strategy['statuses']['value']))
-        print(rev_arbitrage_vals)
-
-        ax.plot(timestamps, arbitrage_vals, label = "arbitrage", color = 'b')
-        ax.plot(timestamps, rev_arbitrage_vals, label = "reverse arbitrage", color = 'g')
-
-#        # A trade order is indicated by a vertical red line
-#        if uri in data['orders']:
-#            min_order_ts = min(data['orders'][uri]['time'])
-#
-#            order_t = [ts - min_order_ts for ts in data['orders'][uri]['time']];
-#            for oi in order_t:
-#                #print("order at: ", xi)
-#                ax.axvline(x = oi, color = 'r')
-
-        ax.legend(); """
-
-
-anim = animation.FuncAnimation(fig, func = animate_strategies, interval = 1000)
-plt.show()
+if switch == '--static':
+    animate_strategies(0)
+    plt.show()
+elif switch == '--dynamic':
+    anim = animation.FuncAnimation(fig, func = animate_strategies, interval = 1000)
+    plt.show()
+else: 
+    sys.exit("Invalid switch given: use either `--static` or `--dynamic`")
